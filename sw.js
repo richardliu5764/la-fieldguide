5879c8400ac01acd8aa26e219047a274a7522829
Richard Liu <richardliu5764@gmail.com>
2026-06-11 00:17:51 -0500

    deploy 20260611-0017

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Field Guide service worker — cache-first shell, app-driven data refresh. */
-var CACHE = "fg-5aef111fab";
+var CACHE = "fg-06b2b7fa69";
 var ASSETS = ["./", "index.html", "trip.enc", "manifest.webmanifest", "sw.js", "icon-180.png", "icon-512.png"];
 
 self.addEventListener("install", function (e) {
